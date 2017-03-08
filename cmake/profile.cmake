41cab9cb808e1808fdf7b43920feeb2a55f2f8c6
Leo Yuriev <leo@yuriev.ru>
2017-03-08 19:24:31 +0300

    fpta: test_memcheck by Valgrind.

--- cmake/profile.cmake
+++ cmake/profile.cmake
@@ -12,7 +12,7 @@ option(ENABLE_GPROF "Enable integration with gprof, a performance analyzing tool
 
 option(ENABLE_VALGRIND "Enable integration with valgrind, a memory analyzing tool" OFF)
 if (ENABLE_VALGRIND)
-    check_include_file(valgrind/valgrind.h HAVE_VALGRIND_VALGRIND_H)
+    check_include_files(valgrind/valgrind.h HAVE_VALGRIND_VALGRIND_H)
     if (NOT HAVE_VALGRIND_VALGRIND_H)
         message (FATAL_ERROR
              "ENABLE_VALGRIND option is set but valgrind/valgrind.h is not found")
