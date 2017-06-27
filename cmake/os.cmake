aeab834614e6d3e1e7413df618b09cee30ab7414
Leo Yuriev <leo@yuriev.ru>
2017-06-28 00:21:42 +0300

    fpta: cleanup CMake formatting (minor).

--- cmake/os.cmake
+++ cmake/os.cmake
@@ -1,7 +1,7 @@
 #
 # Perform operating-system specific configuration.
 #
-if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   set(TARGET_OS_LINUX 1)
   #
   # Enable GNU glibc extentions.
@@ -11,7 +11,7 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   # (see man page for feature_test_macros).
   add_definitions("-D_FILE_OFFSET_BITS=64")
   find_package_message(PLATFORM "Building for Linux" "${CMAKE_SYSTEM_NAME}")
-elseif (${CMAKE_SYSTEM_NAME} STREQUAL "kFreeBSD")
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "kFreeBSD")
   set(TARGET_OS_FREEBSD 1)
   set(TARGET_OS_DEBIAN_FREEBSD 1)
   # Debian/kFreeBSD uses GNU glibc.
@@ -19,26 +19,26 @@ elseif (${CMAKE_SYSTEM_NAME} STREQUAL "kFreeBSD")
   add_definitions("-D_FILE_OFFSET_BITS=64")
   find_package_message(PLATFORM "Building for Debian/kFreeBSD"
     "${CMAKE_SYSTEM_NAME}")
-elseif (${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
   set(TARGET_OS_FREEBSD 1)
   find_package_message(PLATFORM "Building for FreeBSD" "${CMAKE_SYSTEM_NAME}")
-elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
   set(TARGET_OS_DARWIN 1)
 
   #
   # Default build type is None, which uses depends by Apple
   # command line tools. Also supportting install with MacPorts.
   #
-  if (NOT DARWIN_BUILD_TYPE)
+  if(NOT DARWIN_BUILD_TYPE)
     set(DARWIN_BUILD_TYPE None CACHE STRING
       "Choose the type of Darwin build, options are: None, Ports."
       FORCE)
   endif()
 
-  if (${DARWIN_BUILD_TYPE} STREQUAL "Ports")
+  if(${DARWIN_BUILD_TYPE} STREQUAL "Ports")
     # Mac ports get installed into /opt/local, hence:
     include_directories("/opt/local/include")
-    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/opt/local/lib")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L/opt/local/lib")
   endif()
 
   find_package_message(PLATFORM "Building for OS X" "${CMAKE_SYSTEM_NAME}")
@@ -53,7 +53,7 @@ elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
   # this is pretty much irrelevant. Disable CMake rpath features
   # altogether. Suppresses a few warnings.
   set(CMAKE_SKIP_RPATH true)
-elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 else()
-  message (FATAL_ERROR "Unsupported platform -- ${CMAKE_SYSTEM_NAME}")
+  message(FATAL_ERROR "Unsupported platform -- ${CMAKE_SYSTEM_NAME}")
 endif()
