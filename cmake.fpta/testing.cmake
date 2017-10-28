380360c416d894dba1d815b73fa0ddeca6cfcea1
Leo Yuriev <leo@yuriev.ru>
2017-10-28 22:35:08 +0300

    fpta-cmake: fix indentation (minor).

--- cmake.fpta/testing.cmake
+++ cmake.fpta/testing.cmake
@@ -1,19 +1,19 @@
-## Copyright 2010-2017 libfpta authors: please see AUTHORS file.
+##  Copyright 2010-2017 libfpta authors: please see AUTHORS file.
 ##
-## This file is part of libfpta, aka "Fast Positive Tables".
+##  This file is part of libfpta, aka "Fast Positive Tables".
 ##
-## libfpta is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
+##  libfpta is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
 ##
-## libfpta is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+##  libfpta is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with libfpta.  If not, see <http://www.gnu.org/licenses/>.
+##  You should have received a copy of the GNU General Public License
+##  along with libfpta.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
 # Expected GTest was already found and/or pointed via ${gtest_root},
