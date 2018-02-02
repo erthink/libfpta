906283fc71ef664e95f43616104a63554ac25255
Leo Yuriev <leo@yuriev.ru>
2018-02-02 18:55:17 +0300

    fpta: add thread-multi-read ut.

--- test/9thread.cxx
+++ test/9thread.cxx
@@ -36,7 +36,9 @@ static std::string random_string(int len, int seed) {
   return result;
 }
 
-static void thread_proc(fpta_db *db, int thread_num) {
+//------------------------------------------------------------------------------
+
+static void write_thread_proc(fpta_db *db, const int thread_num) {
   SCOPED_TRACE("Thread " + std::to_string(thread_num) + " started");
 
   for (int i = 0; i < 500; ++i) {
@@ -151,12 +153,12 @@ TEST(Threaded, SimpleConcurence) {
   ASSERT_NE(db, (fpta_db *)nullptr);
   SCOPED_TRACE("Database reopened");
 
-  thread_proc(db, 42);
+  write_thread_proc(db, 42);
 
   const int threadNum = 2;
   vector<std::thread> threads;
   for (int16_t i = 1; i <= threadNum; ++i)
-    threads.push_back(std::thread(thread_proc, db, i));
+    threads.push_back(std::thread(write_thread_proc, db, i));
 
   for (auto &it : threads)
     it.join();
@@ -164,6 +166,176 @@ TEST(Threaded, SimpleConcurence) {
   SCOPED_TRACE("All threads are stopped");
   EXPECT_EQ(FPTA_OK, fpta_db_close(db));
 }
+
+//------------------------------------------------------------------------------
+
+static void read_thread_proc(fpta_db *db, const int thread_num,
+                             const int reps) {
+  SCOPED_TRACE("Thread " + std::to_string(thread_num) + " started");
+
+  fpta_name table, ip, port;
+  EXPECT_EQ(FPTA_OK, fpta_table_init(&table, "MyTable"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &ip, "Ip"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &port, "port"));
+
+  fpta_filter filter, filter_a, filter_b;
+  memset(&filter, 0, sizeof(fpta_filter));
+  memset(&filter_a, 0, sizeof(fpta_filter));
+  memset(&filter_b, 0, sizeof(fpta_filter));
+
+  filter.type = fpta_node_and;
+  filter.node_and.a = &filter_a;
+  filter.node_and.b = &filter_b;
+
+  filter_a.type = fpta_node_ne;
+  filter_a.node_cmp.left_id = &ip;
+
+  filter_b.type = fpta_node_ne;
+  filter_b.node_cmp.left_id = &port;
+
+  for (int i = 0; i < reps; ++i) {
+    fpta_txn *txn = nullptr;
+    EXPECT_EQ(FPTA_OK, fpta_transaction_begin(db, fpta_read, &txn));
+    ASSERT_NE(nullptr, txn);
+    // assert(txn->mdbx_txn->mt_dbflags[3] != 0);
+
+    EXPECT_EQ(FPTA_OK, fpta_name_refresh(txn, &table));
+    fpta_name column;
+    EXPECT_EQ(FPTA_OK, fpta_table_column_get(&table, 0, &column));
+
+    std::string str = random_string(15, i);
+    filter_a.node_cmp.right_value = fpta_value_cstr(str.c_str());
+    filter_b.node_cmp.right_value = fpta_value_sint(1000 + (rand() % 1000));
+
+    fpta_cursor *cursor = nullptr;
+    EXPECT_EQ(FPTA_OK, fpta_cursor_open(txn, &column, fpta_value_begin(),
+                                        fpta_value_end(), &filter,
+                                        fpta_unsorted_dont_fetch, &cursor));
+    ASSERT_NE(nullptr, cursor);
+    EXPECT_EQ(FPTA_OK, fpta_cursor_move(cursor, fpta_first));
+    EXPECT_EQ(FPTA_OK, fpta_cursor_eof(cursor));
+    EXPECT_EQ(FPTA_OK, fpta_cursor_close(cursor));
+
+    fpta_name_destroy(&column);
+    EXPECT_EQ(FPTA_OK, fpta_transaction_end(txn, false));
+  }
+  SCOPED_TRACE("Thread " + std::to_string(thread_num) + " finished");
+}
+
+TEST(Threaded, SimpleSelect) {
+  fpta_db *db = nullptr;
+
+  // чистим
+  if (REMOVE_FILE(testdb_name) != 0) {
+    ASSERT_EQ(ENOENT, errno);
+  }
+  if (REMOVE_FILE(testdb_name_lck) != 0) {
+    ASSERT_EQ(ENOENT, errno);
+  }
+
+  EXPECT_EQ(FPTA_OK, fpta_db_open(testdb_name, fpta_weak, fpta_saferam, 0644, 1,
+                                  true, &db));
+  ASSERT_NE(db, (fpta_db *)nullptr);
+  SCOPED_TRACE("Database opened");
+
+  { // create table
+    fpta_column_set def;
+    fpta_column_set_init(&def);
+    EXPECT_EQ(FPTA_OK,
+              fpta_column_describe("Ip", fptu_cstr,
+                                   fpta_primary_unique_ordered_obverse, &def));
+    EXPECT_EQ(FPTA_OK, fpta_column_describe("port", fptu_int64,
+                                            fpta_noindex_nullable, &def));
+    EXPECT_EQ(FPTA_OK, fpta_column_describe("word", fptu_cstr,
+                                            fpta_noindex_nullable, &def));
+    EXPECT_EQ(FPTA_OK, fpta_column_describe(
+                           "_last_changed", fptu_datetime,
+                           fpta_secondary_withdups_ordered_obverse, &def));
+    EXPECT_EQ(FPTA_OK, fpta_column_describe(
+                           "_id", fptu_uint64,
+                           fpta_secondary_unique_ordered_obverse, &def));
+
+    fpta_txn *txn = nullptr;
+    EXPECT_EQ(FPTA_OK, fpta_transaction_begin(db, fpta_schema, &txn));
+    ASSERT_NE(nullptr, txn);
+
+    EXPECT_EQ(FPTA_OK, fpta_table_create(txn, "MyTable", &def));
+    EXPECT_EQ(FPTA_OK, fpta_transaction_end(txn, false));
+    SCOPED_TRACE("Table created");
+  }
+  ASSERT_EQ(FPTA_OK, fpta_db_close(db));
+  db = nullptr;
+  SCOPED_TRACE("Database closed");
+
+  EXPECT_EQ(FPTA_OK, fpta_db_open(testdb_name, fpta_weak, fpta_saferam, 0644, 1,
+                                  false, &db));
+  ASSERT_NE(db, (fpta_db *)nullptr);
+  SCOPED_TRACE("Database reopened");
+
+  fpta_txn *txn = nullptr;
+  fpta_transaction_begin(db, fpta_write, &txn);
+  ASSERT_NE(nullptr, txn);
+
+  fpta_name table, ip, port, word, date, id;
+  EXPECT_EQ(FPTA_OK, fpta_table_init(&table, "MyTable"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &ip, "Ip"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &port, "port"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &word, "word"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &date, "_last_changed"));
+  EXPECT_EQ(FPTA_OK, fpta_column_init(&table, &id, "_id"));
+
+  fptu_rw *tuple = fptu_alloc(5, 1000);
+  ASSERT_NE(nullptr, tuple);
+  EXPECT_EQ(FPTA_OK, fpta_name_refresh(txn, &table));
+
+  EXPECT_EQ(FPTA_OK, fpta_name_refresh_couple(txn, &table, &id));
+  uint64_t result = 0;
+  EXPECT_EQ(FPTA_OK, fpta_table_sequence(txn, &table, &result, 1));
+  EXPECT_EQ(FPTA_OK, fpta_upsert_column(tuple, &id, fpta_value_uint(result)));
+
+  EXPECT_EQ(FPTA_OK, fpta_name_refresh_couple(txn, &table, &ip));
+  EXPECT_EQ(FPTA_OK,
+            fpta_upsert_column(tuple, &ip, fpta_value_cstr("1.1.1.1")));
+
+  EXPECT_EQ(FPTA_OK, fpta_name_refresh_couple(txn, &table, &word));
+  EXPECT_EQ(FPTA_OK,
+            fpta_upsert_column(tuple, &word, fpta_value_cstr("hello")));
+
+  EXPECT_EQ(FPTA_OK, fpta_name_refresh_couple(txn, &table, &port));
+  EXPECT_EQ(FPTA_OK, fpta_upsert_column(tuple, &port, fpta_value_sint(111)));
+
+  EXPECT_EQ(FPTA_OK, fpta_name_refresh_couple(txn, &table, &date));
+  EXPECT_EQ(FPTA_OK, fpta_upsert_column(tuple, &date,
+                                        fpta_value_datetime(fptu_now_fine())));
+
+  EXPECT_EQ(FPTA_OK, fpta_probe_and_upsert_row(txn, &table, fptu_take(tuple)));
+
+  EXPECT_EQ(FPTA_OK, fptu_clear(tuple));
+  free(tuple);
+
+  fpta_name_destroy(&table);
+  fpta_name_destroy(&id);
+  fpta_name_destroy(&word);
+  fpta_name_destroy(&ip);
+  fpta_name_destroy(&port);
+  fpta_name_destroy(&date);
+
+  EXPECT_EQ(FPTA_OK, fpta_transaction_end(txn, false));
+  SCOPED_TRACE("Record written");
+
+  const int threadNum = 42;
+  vector<std::thread> threads;
+  for (int i = 0; i < threadNum; ++i)
+    threads.push_back(std::thread(read_thread_proc, db, i, 42 * 1000));
+
+  for (auto &thread : threads)
+    thread.join();
+
+  SCOPED_TRACE("All threads are stopped");
+  EXPECT_EQ(FPTA_OK, fpta_db_close(db));
+}
+
+//------------------------------------------------------------------------------
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
